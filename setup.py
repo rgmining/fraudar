6dbc9b54610f34fb92bb46ef7dda28dc172fe5d7
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-10-19 03:01:11 +0900

    Add package information.

---
+++ setup.py
@@ -0,0 +1,59 @@
+#
+# setup.py
+#
+# Copyright (c) 2016 Junpei Kawamoto
+#
+# This file is part of rgmining-fraudar.
+#
+# rgmining-fraudar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# rgmining-fraudar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Package information about a wrapper of Fraudar algorithm.
+"""
+from setuptools import setup, find_packages
+
+
+def _load_requires_from_file(filepath):
+    """Read a package list from a given file path.
+
+    Args:
+      filepath: file path of the package list.
+
+    Returns:
+      a list of package names.
+    """
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
+
+setup(
+    name='rgmining-fraudar',
+    version='0.1.0',
+    author="Junpei Kawamoto",
+    author_email="kawamoto.junpei@gmail.com",
+    description="A wrapper of Fraudar algorithm for Review graph mining project",
+    url="https://github.com/rgmining/fraudar",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=_load_requires_from_file("requirements.txt"),
+    test_suite='tests.suite',
+    license="GPLv3",
+    classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Natural Language :: English",
+            "Programming Language :: Python",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Scientific/Engineering :: Information Analysis"
+        ]
+)
