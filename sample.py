dc205adce545b6aa068c2ebd295d293583f94d7e
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-01-05 01:13:28 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 #
-# sample.py
+#  sample.py
 #
-# Copyright (c) 2016-2017 Junpei Kawamoto
+#  Copyright (c) 2016-2023 Junpei Kawamoto
 #
-# This file is part of rgmining-fraudar.
+#  This file is part of rgmining-fraudar.
 #
-# rgmining-fraudar is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#  rgmining-fraudar is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# rgmining-fraudar is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  rgmining-fraudar is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with rgmining-fraudar. If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with rgmining-fraudar. If not, see <http://www.gnu.org/licenses/>.
 #
 """A simple sample script running FRAUDAR with a synthetic data set.
 
@@ -29,9 +29,9 @@ To install that package, run
     $ pip install --upgrade rgmining-synthetic-dataset
 
 """
-from __future__ import print_function, division
-from os import path
 import sys
+from os import path
+
 import click
 import synthetic  # pylint: disable=import-error
 
@@ -55,9 +55,7 @@ def analyze(blocks):
     detected = [r for r in graph.reviewers if r.anomalous_score]
     correct = [r for r in detected if "anomaly" in r.name]
 
-    print(
-        len(correct) / len(detected),
-        len(correct) / synthetic.ANOMALOUS_REVIEWER_SIZE)
+    print(len(correct) / len(detected), len(correct) / synthetic.ANOMALOUS_REVIEWER_SIZE)
 
 
 if __name__ == "__main__":
